9950fea953c129bd5754859e999a8eb96ae6cfa3
whitleywspencer <whitley.spencer@galvanize.com>
2023-11-22 15:12:32 -0500

    Task 2 complete

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showData: boolean
 }
 
 /**
@@ -22,26 +23,41 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showData: false
     };
   }
 
   /**
+   * If the showData property is true,
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showData) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
+   * call to get data is sent every 100 miliseconds
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
-  }
+    let time = 0
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({
+          data: serverResponds,
+          showData: true
+        });
+      });
+      time ++
+      if (time > 1000) {
+        clearInterval(interval)
+      }
+    }, 100)
+  };
 
   /**
    * Render the App react component
@@ -53,15 +69,15 @@ class App extends Component<{}, IState> {
           Bank & Merge Co Task 2
         </header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
-            Start Streaming Data
-          </button>
+            <button className="btn btn-primary Stream-button"
+              // when button is click, our react app tries to request
+              // new data from the server.
+              // As part of your task, update the getDataFromServer() function
+              // to keep requesting the data every 100ms until the app is closed
+              // or the server does not return anymore data.
+              onClick={() => {this.getDataFromServer()}}>
+              Start Streaming Data
+            </button>
           <div className="Graph">
             {this.renderGraph()}
           </div>
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem= document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -46,9 +46,25 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
       elem.load(this.table);
+      // declare object with additional configurations to be set
+      const config = {
+        'view': 'y_line',
+        'column-pivots': '["stock"]',
+        'row-pivots': '["timestamp"]',
+        'columns': '["top_ask_price"]',
+        'aggregates': `{
+          "stock": "distinct count",
+          "top_ask_price": "avg",
+          "top_bid_price": "avg",
+          "timestamp": "distinct count"
+        }`,
+      }
+      // Add more Perspective configurations here.
+      Object.entries(config).forEach(([prop, val])=> {
+        console.log(prop, val)
+        elem.setAttribute(prop, val)
+      })
     }
   }
 
